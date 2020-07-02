fd354e5b00357bbf08073d79bdc7dd78ebbb24c5
neks <tkmail2005@gmail.com>
2020-07-02 19:28:41 +0200

    Initial Commit - Stable

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    console.log("Handling / request...");
+
+    res.sendFile(__dirname + "/templates/home.html");
+});
+
+app.get("/events", function(req, res) {
+    console.log("Handling /events request...");
+
+    res.sendFile(__dirname + "/templates/events.html");
+});
+
+app.get("/query", function(req, res) {
+    console.log("Handling /query request with query [" + JSON.stringify(req.query) + "]...");
+
+    const query = req.query;
+    const events = JSON.parse(fs.readFileSync("events.json"));
+
+    // Return Full List
+    if(Object.keys(query).length == 0) {
+        res.send(events);
+        return;
+    }
+
+    // Set Up Filter Criteria
+    let search_name = query.name.toLowerCase();
+    let search_types = query.types.split(",");
+
+    let data = events;
+    
+    // Filter Name
+    if(query.name != "") {
+        data = data.filter(i => i.name.toLowerCase().includes(search_name));
+    }
+    // Filter Type
+    if(search_types.length != 5) {
+        data = data.filter(i => search_types.includes(i.type));
+    }
+
+    // Send Filtered Data
+    res.send(data);
+});
+
+app.listen(80, "localhost", function() {
+    console.log("Started Web Server On Port 80...");
+});
