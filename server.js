a9da766b27971b3a9a39c8d36f82e35d146a3f77
Neks <tkmail2005@gmail.com>
2020-07-12 20:43:54 +0200

    Moved query to own function - Stable

--- server.js
+++ server.js
@@ -20,33 +20,42 @@ app.get("/query", function(req, res) {
     console.log("Handling /query request with query [" + JSON.stringify(req.query) + "]...");
 
     const query = req.query;
-    const events = JSON.parse(fs.readFileSync("events.json"));
-
-    // Return Full List
-    if(Object.keys(query).length == 0) {
-        res.send(events);
-        return;
-    }
-
-    // Set Up Filter Criteria
-    let search_name = query.name.toLowerCase();
-    let search_types = query.types.split(",");
-
-    let data = events;
-    
-    // Filter Name
-    if(query.name != "") {
-        data = data.filter(i => i.name.toLowerCase().includes(search_name));
-    }
-    // Filter Type
-    if(search_types.length != 5) {
-        data = data.filter(i => search_types.some(j => i.types.includes(j)));
-    }
 
     // Send Filtered Data
-    res.send(data);
+    res.send(getEvents(query));
 });
 
 app.listen(8080, "localhost", function() {
     console.log("Started Web Server On Port 8080...");
 });
+
+function getEvents(query) {
+    const events = JSON.parse(fs.readFileSync("events.json"));
+
+    if(Object.keys(query).length == 0) {
+        // Empty Query
+        return events;
+    }
+
+    // Set Up Filter Criteria
+    let search_name = query.name != "" ? query.name : false;
+    if(search_name) search_name = search_name.toLowerCase();
+
+    let search_types = query.types != "" ? query.types : false;
+    if(search_types) search_types = search_types.split(",");
+
+    let data = events;
+    
+    // Filter Name
+    if(search_name) {
+        data = data.filter(i => i.name.toLowerCase().includes(search_name));
+    }
+    // Filter Type
+    if(search_types) {
+        data = data.filter(i => i.types.some(j => search_types.includes(j)));
+    } else {
+        data = [];
+    }
+
+    return data;
+}
